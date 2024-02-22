cf5d6fe4b9a3c300982ec6ce2a574306a700e75c
Leonid Frolov <froleod@gmail.com>
2024-02-22 13:11:37 +0300

    upd prak4

--- src/prak4/MyExecutorService.java
+++ src/prak4/MyExecutorService.java
@@ -1,12 +1,54 @@
 package prak4;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Executor;
+import java.util.concurrent.LinkedBlockingQueue;
 
-public class MyExecutorService {
-//    ExecutorService executor = Executors.newSingleThreadExecutor();
-//        executor.submit(()-> {
-//        String threadName = Thread.currentThread().getName();
-//        System.out.println("Hello " + threadName);
-//    });
+public class MyExecutorService implements Executor {
+    private final BlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<>();
+    private final Thread[] threads;
+
+    public MyExecutorService(int numThreads) {
+        this.threads = new Thread[numThreads];
+        for (int i = 0; i < numThreads; i++) {
+            threads[i] = new Thread(this::taskExecutor);
+            threads[i].start();
+        }
+    }
+
+    private void taskExecutor() {
+        while (true) {
+            try {
+                Runnable task = workQueue.take();
+                task.run();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void execute(Runnable command) {
+        try {
+            workQueue.put(command);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public void shutdown() {
+        for (Thread thread : threads) {
+            thread.interrupt();
+        }
+    }
+    public static void main(String[] args) {
+        MyExecutorService executorService = new MyExecutorService(2);
+        executorService.execute(() -> System.out.println("Message 1"));
+        executorService.execute(() -> System.out.println("Message 2"));
+        executorService.execute(() -> System.out.println("Message 3"));
+        executorService.execute(() -> System.out.println("Message 4"));
+        executorService.execute(() -> System.out.println("Message 5"));
+        executorService.shutdown();
+    }
 }
---
+++ src/prak5/Test.java
@@ -0,0 +1,7 @@
+package prak5;
+
+public class Test {
+    public static void main(String[] args) {
+//        Singleton1 singleton1 = new Singleton1();
+    }
+}
